2051299ac6fdd35fbdc0ad41c6d39c6b4655f0b4
SPFarm <SPFarm@dev.pobaldev>
2016-08-24 12:13:24 +0000

    Added the Entity Framework Core

--- Repository/PersistenceContext.cs
+++ Repository/PersistenceContext.cs
@@ -27,12 +27,12 @@ namespace tccp.Reposotiry
 
         private void SetCustomer(ModelBuilder constructorModel)
         {
-            constructorModel.Entity<Cliente>( etd=>
+            constructorModel.Entity<Customer>( etd=>
             {
-                etd.ToTable("tbCliente");
-                etd.HasKey(c => c.Id).HasName("idCliente");
-                etd.Property(c => c.Id).HasColumnName("idCliente").ValueGeneratedOnAdd();
-                etd.Property(c => c.Nome).HasColumnName("nome").HasMaxLength(100);
+                etd.ToTable("tblCustomer");
+                etd.HasKey(c => c.Id).HasName("idCustomer");
+                etd.Property(c => c.Id).HasColumnName("idCustomer").ValueGeneratedOnAdd();
+                etd.Property(c => c.Nome).HasColumnName("name").HasMaxLength(100);
                 etd.Property(c => c.Email).HasColumnName("email").HasMaxLength(30);
             });                
         }
@@ -41,11 +41,11 @@ namespace tccp.Reposotiry
         {
             constructorModel.Entity<Pedido>(etd =>
             {
-                etd.ToTable("tbPedido");
-                etd.HasKey(p => p.Id).HasName("idPedido");
-                etd.Property(p => p.Id).HasColumnName("idPedido").ValueGeneratedOnAdd();                                
+                etd.ToTable("tblOrder");
+                etd.HasKey(p => p.Id).HasName("idOrder");
+                etd.Property(p => p.Id).HasColumnName("idOrder").ValueGeneratedOnAdd();                                
                 etd.Property(p => p.Total).HasColumnName("total");
-                etd.HasOne(c => c.Cliente).WithMany(p => p.Pedidos);
+                etd.HasOne(c => c.Customer).WithMany(p => p.Orders);
             });
         }
     }
---
+++ Repository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tccp.Reposotiry
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
